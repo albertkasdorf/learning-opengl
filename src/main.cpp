a9c1f1925201e5aec4221a7a81ba311d3a7d19dd
Albert Kasdorf <albert.kasdorf@live.de>
2023-11-15 20:54:14 +0100

    OSX adaption added.

--- src/main.cpp
+++ src/main.cpp
@@ -45,6 +45,7 @@ auto main(int argc, char** argv) -> int
     // Configure GLFW
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GLFW_TRUE);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     // Create a window object
